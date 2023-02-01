c6c953780deb2ca87b172e6f2669193bcb0490c4
Thiagohrcosta <thiagohrcosta86@gmail.com>
2023-01-31 21:54:54 -0300

    adding server and params

---
+++ src/routes.js
@@ -0,0 +1,4 @@
+import { randomUUID } from 'node:crypto'
+import { buildRoutePath } from './utils/build-route-path.js'
+
+const database = new Database()
---
+++ src/server.js
@@ -0,0 +1,3 @@
+import http from 'node:http'
+
+import { routes } from './routes.js'
