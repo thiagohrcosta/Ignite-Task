f9b1038ff02161cfedd82b60fa0f6b7887b8f735
Thiagohrcosta <thiagohrcosta86@gmail.com>
2023-02-01 22:46:58 -0300

    adding post request

--- src/routes.js
+++ src/routes.js
@@ -31,6 +31,21 @@ export const routes = [
           JSON.stringify({ message: "description is required"})
         )
       }
+
+      const task = {
+        id: randomUUID(),
+        title,
+        description,
+        completed_at: null,
+        created_at: new Date(),
+        updated_at: new Date()
+      }
+
+      console.log(task)
+
+      database.insert('task', task)
+      return res.writeHead(201).end()
+
     }
   }
 ]
